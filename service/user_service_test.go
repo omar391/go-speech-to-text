1d98ff1d6b6d62266c2e5c1c7885ef2e038fe9de
Omar <omar391@gmail.com>
2021-04-08 18:04:56 +0600

    Updated test suites

--- service/user_service_test.go
+++ service/user_service_test.go
@@ -1,16 +1,13 @@
 package service
 
 import (
-	"fmt"
 	"stt-service/models"
 	"testing"
-
-	"github.com/go-playground/assert/v2"
 )
 
 func TestUserCreation(m *testing.T) {
 	AddNewUser(&models.User{Email: "ding@dong.om", Name: "wow", Password: GeneratePasswordHash("")})
-	s := []byte("")
+	// s := []byte("")
 
-	assert len(s)!=0
+	// assert len(s)!=0
 }
