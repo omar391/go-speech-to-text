515bce25f43afae231a074b2d6d9475164dbb5be
Omar <omar391@gmail.com>
2021-04-13 17:51:22 +0600

    Fixed heroku PORT config

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
 	"stt-service/conf"
 	"stt-service/router"
 )
 
 func main() {
 	//	starting the http routers with the address/port set in the config file
-	if err := router.Start(conf.Config.ADDR); err != nil {
+	addr := conf.Config.ADDR
+	if os.Getenv("PORT") != "" {
+		addr = ":" + addr
+	}
+
+	if err := router.Start(addr); err != nil {
 		log.Println("start: ", err)
 	}
 }
